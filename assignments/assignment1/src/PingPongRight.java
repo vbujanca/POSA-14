f689b0ebf6ea72f33c40c7c8ec7e7c5c33cdee87
Vali Bujanca <vbujanca@yahoo.com>
2014-05-15 18:17:54 -0400

    assignment 1

--- assignments/assignment1/src/PingPongRight.java
+++ assignments/assignment1/src/PingPongRight.java
@@ -19,7 +19,7 @@ public class PingPongRight {
     /**
      * Latch that will be decremented each time a thread exits.
      */
-    public static CountDownLatch latch = null; // TODO - You fill in here
+    public static CountDownLatch latch = new CountDownLatch(2); // TODO - You fill in here
 
     /**
      * @class PlayPingPongThread
@@ -33,9 +33,13 @@ public class PingPongRight {
         /**
          * Constructor initializes the data member.
          */
-        public PlayPingPongThread (/* TODO - You fill in here */)
+        public PlayPingPongThread (/* TODO - You fill in here */String name, SimpleSemaphore pingSemaphore, SimpleSemaphore pongSemaphore)
         {
             // TODO - You fill in here.
+            this.name = name;
+            this.pingSemaphore = pingSemaphore;
+            this.pongSemaphore = pongSemaphore;
+
         }
 
         /**
@@ -46,6 +50,25 @@ public class PingPongRight {
         public void run () 
         {
             // TODO - You fill in here.
+
+
+
+            for(int i = 1; i <= mMaxIterations; i++){
+                try {
+                    this.pingSemaphore.acquireUninterruptibly();
+                    System.out.println(this.name + " ("+i+")");
+
+                    this.pongSemaphore.release();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            latch.countDown();
+
+
         }
 
         /**
@@ -53,11 +76,14 @@ public class PingPongRight {
          * iteration.
          */
         // TODO - You fill in here.
+        private String name;
 
         /**
          * The two SimpleSemaphores use to alternate pings and pongs.
          */
         // TODO - You fill in here.
+        SimpleSemaphore pingSemaphore;
+        SimpleSemaphore pongSemaphore;
     }
 
     /**
@@ -70,14 +96,18 @@ public class PingPongRight {
 
             // TODO - You fill in here.
 
+
             System.out.println("Ready...Set...Go!");
 
             // Create the ping and pong threads, passing in the string
             // to print and the appropriate SimpleSemaphores.
+            SimpleSemaphore pingSemaphore = new SimpleSemaphore(1, false);
+            SimpleSemaphore pongSemaphore = new SimpleSemaphore(0, false);
+
             PlayPingPongThread ping =
-                new PlayPingPongThread(/* TODO - You fill in here */);
+                new PlayPingPongThread(/* TODO - You fill in here */ "Ping", pingSemaphore, pongSemaphore);
             PlayPingPongThread pong =
-                new PlayPingPongThread(/* TODO - You fill in here */);
+                new PlayPingPongThread(/* TODO - You fill in here */ "Pong", pongSemaphore,pingSemaphore);
             
             // Initiate the ping and pong threads, which will call the
             // run() hook method.
@@ -90,6 +120,8 @@ public class PingPongRight {
             // TODO - replace replace the following line with a
             // CountDownLatch barrier synchronizer call that waits for
             // both threads to finish.
+            latch.await();
+
             throw new java.lang.InterruptedException();
         } 
         catch (java.lang.InterruptedException e)
--- assignments/assignment1/src/SimpleSemaphore.java
+++ assignments/assignment1/src/SimpleSemaphore.java
@@ -1,6 +1,5 @@
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
@@ -8,16 +7,21 @@ import java.util.concurrent.locks.Condition;
  * @brief This class provides a simple counting semaphore
  *        implementation using Java a ReentrantLock and a
  *        ConditionObject.  It must implement both "Fair" and
- *        "NonFair" semaphore semantics, just liked Java Semaphores. 
+ *        "NonFair" semaphore semantics, just liked Java Semaphores.
  */
 public class SimpleSemaphore {
+
     /**
-     * Constructor initialize the data members.  
+     * Constructor initialize the data members.
      */
     public SimpleSemaphore (int permits,
                             boolean fair)
-    { 
+    {
         // TODO - you fill in here
+        this.permits = permits;
+        this.lock = new ReentrantLock(fair);
+        this.outOfPermits = this.lock.newCondition();
+
     }
 
     /**
@@ -26,6 +30,14 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here
+        this.lock.lockInterruptibly();
+        try{
+            while(this.permits == 0) this.outOfPermits.await();
+            this.permits --;
+            outOfPermits.signalAll();
+        }finally {
+            this.lock.unlock();
+        }
     }
 
     /**
@@ -34,6 +46,14 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here
+        this.lock.lock();
+        try{
+            while(this.permits == 0) this.outOfPermits.awaitUninterruptibly();
+           this.permits --;
+            outOfPermits.signal();
+        }  finally {
+            this.lock.unlock();
+        }
     }
 
     /**
@@ -41,22 +61,33 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here
+        this.lock.lock();
+        try{
+            this.permits ++;
+            outOfPermits.signal();
+
+        }finally {
+            this.lock.unlock();
+        }
     }
 
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+    private ReentrantLock lock;
 
     /**
      * Define a ConditionObject to wait while the number of
      * permits is 0.
      */
     // TODO - you fill in here
+    Condition outOfPermits;
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here
+    private int permits;
 }
 
