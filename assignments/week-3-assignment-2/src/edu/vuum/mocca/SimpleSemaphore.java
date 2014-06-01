f4464fbe649c42dc883f07dcad9bce8af2ac19a9
Vali Bujanca <vbujanca@yahoo.com>
2014-05-31 21:23:51 -0400

    Merge branch 'master' of https://github.com/douglascraigschmidt/POSA-14
    Conflicts:
    	assignments/week-2-assignment-1/src/edu/vuum/mocca/SimpleAtomicLong.java
    	assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleAtomicLong.java
    	assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
    	assignments/week-4-assignment-3/src/PingPongRight.java
    	assignments/week-4-assignment-3/src/SimpleSemaphore.java

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,35 +1,52 @@
+package edu.vuum.mocca;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
- *
+ * 
  * @brief This class provides a simple counting semaphore
  *        implementation using Java a ReentrantLock and a
- *        ConditionObject.  It must implement both "Fair" and
- *        "NonFair" semaphore semantics, just liked Java Semaphores. 
+ *        ConditionObject (which is accessed via a Condition). It must
+ *        implement both "Fair" and "NonFair" semaphore semantics,
+ *        just liked Java Semaphores.
  */
 public class SimpleSemaphore {
+    /**
+     * Define a ReentrantLock to protect the critical section.
+     */
+    // TODO - you fill in here
+    private ReentrantLock lock;
 
     /**
-     * Constructor initialize the data members.  
+     * Define a Condition that waits while the number of permits is 0.
      */
-    public SimpleSemaphore (int permits,
-                            boolean fair)
-    { 
-        // TODO - you fill in here
+    // TODO - you fill in here
+    Condition outOfPermits;
+
+    /**
+     * Define a count of the number of available permits.
+     */
+    // TODO - you fill in here.  Make sure that this data member will
+    // ensure its values aren't cached by multiple Threads..
+    private int permits;
+
+    public SimpleSemaphore(int permits, boolean fair) {
+        // TODO - you fill in here to initialize the SimpleSemaphore,
+        // making sure to allow both fair and non-fair Semaphore
+        // semantics.
         this.permits = permits;
         this.lock = new ReentrantLock(fair);
         this.outOfPermits = this.lock.newCondition();
-
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that can
-     * be interrupted.
+     * Acquire one permit from the semaphore in a manner that can be
+     * interrupted.
      */
     public void acquire() throws InterruptedException {
-        // TODO - you fill in here
+        // TODO - you fill in here.
         this.lock.lockInterruptibly();
         try{
             while(this.permits == 0) this.outOfPermits.await();
@@ -41,11 +58,11 @@ public class SimpleSemaphore {
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that
-     * cannot be interrupted.
+     * Acquire one permit from the semaphore in a manner that cannot be
+     * interrupted.
      */
     public void acquireUninterruptibly() {
-        // TODO - you fill in here
+        // TODO - you fill in here.
         this.lock.lock();
         try{
             while(this.permits == 0) this.outOfPermits.awaitUninterruptibly();
@@ -60,7 +77,7 @@ public class SimpleSemaphore {
      * Return one permit to the semaphore.
      */
     void release() {
-        // TODO - you fill in here
+        // TODO - you fill in here.
         this.lock.lock();
         try{
             this.permits ++;
@@ -72,22 +89,11 @@ public class SimpleSemaphore {
     }
 
     /**
-     * Define a ReentrantLock to protect the critical section.
+     * Return the number of permits available.
      */
-    // TODO - you fill in here
-    private ReentrantLock lock;
-
-    /**
-     * Define a ConditionObject to wait while the number of
-     * permits is 0.
-     */
-    // TODO - you fill in here
-    Condition outOfPermits;
-
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here
-    private int permits;
+    public int availablePermits() {
+        // TODO - you fill in here by changing null to the appropriate
+        // return value.
+        return this.permits;
+    }
 }
-
